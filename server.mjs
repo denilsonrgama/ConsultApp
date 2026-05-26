cdbee87e69b749ccf55b45813a8f72051f47c735
DenilsonGama <drogama@outlook.com>
2026-05-26 11:26:43 -0300

    Relatórios para evitar barra horizontal quando for possível

--- server.mjs
+++ server.mjs
@@ -49,7 +49,7 @@ const pythonExe =
 const port = Number(process.env.PORT || 5173);
 const host = process.env.HOST || "0.0.0.0";
 
-const serverVersion = "v206";
+const serverVersion = "v207";
 
 const postgresConnectionString = databaseConnectionString();
 
